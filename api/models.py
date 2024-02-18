39d480151ce67a51df77835dbdee468b8d8e8771
Le Nhu Nghia Nguyen (グエン・レ・ニュ・ギア) <92531311+Kay-Day@users.noreply.github.com>
2024-02-19 00:47:41 +0700

    Update BackEnd

--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+
 
 class User(AbstractUser):
     username = models.CharField(max_length=100)
--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -128,6 +130,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = { 
     'DEFAULT_AUTHENTICATION_CLASSES': (
